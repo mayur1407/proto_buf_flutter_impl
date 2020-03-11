359a5c3081f70e7d10fb6b3281a5b9f3a039ef2c
Mayur Sharma <mayur.sharma_1@signify.com>
2020-03-11 16:53:55 +0530

    updated dart interface code to get serialized data

--- lib/proto_buf_flutter_impl.dart
+++ lib/proto_buf_flutter_impl.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
-
+import 'dart:typed_data';
 import 'package:flutter/services.dart';
+import 'package:proto_buf_flutter_impl/gen/protos/employee.pb.dart';
 
 class ProtoBufFlutterImpl {
   static const MethodChannel _channel =
       const MethodChannel('proto_buf_flutter_impl');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<Employee> get platformVersion async {
+    final Uint8List employeeRawData = await _channel.invokeMethod('getPlatformVersion');
+    final Employee employee = Employee.fromBuffer(employeeRawData);
+    return employee;
   }
 }
